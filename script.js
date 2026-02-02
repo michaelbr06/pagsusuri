511ff4bc297a115aa519a54624a96c8be500fb07
Michael BR <michaelbr@duck.com>
2026-02-02 17:08:03 +0800

    Add more sins to the First Commandment
    Ref: From Handbook of Prayers by Charles Belmonte-James Socias.

--- script.js
+++ script.js
@@ -11,11 +11,36 @@ const translations = {
         id: "c1",
         header: "1st Commandment",
         items: [
-          { id: "s1", text: "I have neglected my daily prayers." },
-          { id: "s2", text: "I have put money or work before God." },
+          {
+            id: "s1",
+            text: "I performed my duties towards God reluctantly or grudgingly.",
+          },
+          { id: "s2", text: "I failed to recite my usual prayers." },
           {
             id: "s3",
-            text: "I have performed my duties towards God reluctantly or grudgingly.",
+            text: "I received Holy Communion in the state of mortal sin or without the necessary preparation.",
+          },
+          { id: "s4", text: "I missed the one-hour Eucharistic fast." },
+          {
+            id: "s5",
+            text: "I failed to mention a grave sin in my previous confessions.",
+          },
+          {
+            id: "s6",
+            text: "I believed in superstition or engaged in superstitious practices (fortune-telling, horoscopes, faith-healing, etc.).",
+          },
+          { id: "s7", text: "I harbored serious doubts in matters of Faith." },
+          {
+            id: "s8",
+            text: "I put my Faith in danger by reading books, pamphlets, or magazines containing errors or things contrary to Catholic faith and morals.",
+          },
+          {
+            id: "s9",
+            text: "I endangered my Faith by joining or attending meetings and activities of organizations contrary to the Church or the Catholic faith.",
+          },
+          {
+            id: "s10",
+            text: "I committed the sin of sacrilege by profaning a sacred person, place, or thing.",
           },
         ],
       },
